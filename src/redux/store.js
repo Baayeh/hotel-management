01a0440780628ccb2b8859de234ffb97051f1476
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2022-11-26 10:39:38 +0000

    Install redux logger middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
 import clientSlice from './booking/client-details/clientSlice';
 
 const store = configureStore({
-  reducers: {
+  reducer: {
     client: clientSlice,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
