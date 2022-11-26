c966367fa350685e47c04a48125a2bfba07bcded
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2022-11-26 09:47:10 +0000

    Disable button when form is invalid

--- src/components/reservation/ClientDetails.jsx
+++ src/components/reservation/ClientDetails.jsx
@@ -37,101 +37,105 @@ const ClientDetails = ({ activeStep, steps, handleNext, handleBack }) => {
         validationSchema={validationSchema}
         onSubmit={onSubmit}
       >
-        <Form className="my-4">
-          <div className="client-name flex gap-4 w-[45rem] mx-auto justify-center">
-            <Field name="fname">
-              {(props) => {
-                const { field, meta } = props;
-                return (
-                  <TextField
-                    id="fname"
-                    fullWidth
-                    label="First Name"
-                    {...field}
-                    variant="outlined"
-                    size="small"
-                    error={meta.touched && meta.error ? !isError : isError}
-                    helperText={meta.touched && meta.error}
-                    required
-                  />
-                );
-              }}
-            </Field>
-            <Field name="lname">
-              {(props) => {
-                const { field, meta } = props;
-                return (
-                  <TextField
-                    id="lname"
-                    fullWidth
-                    label="Last Name"
-                    {...field}
-                    variant="outlined"
-                    size="small"
-                    error={meta.touched && meta.error ? !isError : isError}
-                    helperText={meta.touched && meta.error}
-                    required
-                  />
-                );
-              }}
-            </Field>
-          </div>
+        {(props) => {
+          const { isValid } = props;
+          return (
+            <Form className="my-4">
+              <div className="client-name flex gap-4 w-[45rem] mx-auto justify-center">
+                <Field name="fname">
+                  {(props) => {
+                    const { field, meta } = props;
+                    return (
+                      <TextField
+                        id="fname"
+                        fullWidth
+                        label="First Name"
+                        {...field}
+                        variant="outlined"
+                        size="small"
+                        error={meta.touched && meta.error ? !isError : isError}
+                        helperText={meta.touched && meta.error}
+                        required
+                      />
+                    );
+                  }}
+                </Field>
+                <Field name="lname">
+                  {(props) => {
+                    const { field, meta } = props;
+                    return (
+                      <TextField
+                        id="lname"
+                        fullWidth
+                        label="Last Name"
+                        {...field}
+                        variant="outlined"
+                        size="small"
+                        error={meta.touched && meta.error ? !isError : isError}
+                        helperText={meta.touched && meta.error}
+                        required
+                      />
+                    );
+                  }}
+                </Field>
+              </div>
 
-          <div className="client-contact my-4 flex gap-4 w-[45rem] mx-auto justify-center">
-            <Field name="email">
-              {(props) => {
-                const { field, meta } = props;
-                return (
-                  <TextField
-                    id="email"
-                    fullWidth
-                    label="Email"
-                    {...field}
-                    variant="outlined"
-                    size="small"
-                    error={meta.touched && meta.error ? !isError : isError}
-                    helperText={meta.touched && meta.error}
-                    required
-                  />
-                );
-              }}
-            </Field>
-            <Field name="pnumber">
-              {(props) => {
-                console.log(props);
-                const { field, meta } = props;
-                return (
-                  <TextField
-                    id="pnumber"
-                    fullWidth
-                    label="Phone Number"
-                    {...field}
-                    variant="outlined"
-                    size="small"
-                    error={meta.touched && meta.error ? !isError : isError}
-                    helperText={meta.touched && meta.error}
-                    required
-                  />
-                );
-              }}
-            </Field>
-          </div>
+              <div className="client-contact my-4 flex gap-4 w-[45rem] mx-auto justify-center">
+                <Field name="email">
+                  {(props) => {
+                    const { field, meta } = props;
+                    return (
+                      <TextField
+                        id="email"
+                        fullWidth
+                        label="Email"
+                        {...field}
+                        variant="outlined"
+                        size="small"
+                        error={meta.touched && meta.error ? !isError : isError}
+                        helperText={meta.touched && meta.error}
+                        required
+                      />
+                    );
+                  }}
+                </Field>
+                <Field name="pnumber">
+                  {(props) => {
+                    const { field, meta } = props;
+                    return (
+                      <TextField
+                        id="pnumber"
+                        fullWidth
+                        label="Phone Number"
+                        {...field}
+                        variant="outlined"
+                        size="small"
+                        error={meta.touched && meta.error ? !isError : isError}
+                        helperText={meta.touched && meta.error}
+                        required
+                      />
+                    );
+                  }}
+                </Field>
+              </div>
 
-          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-            <Button
-              color="inherit"
-              disabled={activeStep === 0}
-              onClick={handleBack}
-              sx={{ mr: 1 }}
-            >
-              Back
-            </Button>
-            <Box sx={{ flex: '1 1 auto' }} />
-            <Button type="submit">
-              {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-            </Button>
-          </Box>
-        </Form>
+              <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+                <Button
+                  color="inherit"
+                  disabled={activeStep === 0}
+                  onClick={handleBack}
+                  sx={{ mr: 1 }}
+                >
+                  Back
+                </Button>
+                <Box sx={{ flex: '1 1 auto' }} />
+                <Button type="submit" disabled={!isValid}>
+                  {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+                </Button>
+              </Box>
+            </Form>
+          );
+        }}
       </Formik>
     </section>
   );
